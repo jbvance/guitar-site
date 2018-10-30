e400d8c3de41d9342b83255a753694f14e966af2
Jason Vance <=>
2018-10-29 20:37:43 -0500

    begin adding user model

--- server/server.js
+++ server/server.js
@@ -13,6 +13,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Models
+const { User } = require('./models/user');
+
+//======================================
+//             USERS 
+//======================================
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save()
+    .then(doc => {
+        res.status(200).json({
+            success: true,
+            userdata: doc
+        })
+    })
+    .catch(err => res.json({ success: false, err }));
+});
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
