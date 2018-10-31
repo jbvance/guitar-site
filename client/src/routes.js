d0324a0ae6f1d306dd26f77f6f38d2e5367ac2aa
Jason Vance <=>
2018-10-31 07:45:41 -0500

    begin user registration

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,12 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Layout from './hoc/layout';
 import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/register';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        
         <Route path="/register_login" exact component={RegisterLogin}/>
+        <Route path="/register" exact component={Register}/>
         <Route path="/" exact component={Home} />        
       </Switch>
     </Layout>
