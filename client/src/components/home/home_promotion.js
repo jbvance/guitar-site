f4d4c7c628f0559a2f2901cb5418e1e656d8d97e
Jason Vance <=>
2018-11-01 13:32:18 -0500

    add home slider and promotion components

---
+++ client/src/components/home/home_promotion.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '../utils/button';
+
+const HomePromotion = (props) => {
+    const promotion =  {
+        img: '/images/featured/featured_home_3.jpg',
+        lineOne: 'Up to 40% off',
+        lineTwo: 'In second-hand guitars',
+        linkTitle: 'Shop now',
+        linkTo: '/shop'
+    }
+
+    const renderPromotion = () => (
+        promotion ? 
+            <div className="home_promotion_img"
+                style={{
+                    background: `url(${promotion.img})`
+                }}
+            >
+                <div className="tag title">
+                    {promotion.lineOne}
+                </div>
+                <div className="tag low_title">
+                    {promotion.lineTwo}
+                </div>
+                <Button 
+                    type="default"
+                    title={promotion.linkTitle}
+                    linkTo={promotion.linkTo}
+                    addStyles={{
+                        margin: '10px 0 0 0'
+                    }}
+                />
+            </div>
+        : null
+    );
+
+    return (
+        <div className="home_promotion">
+            {renderPromotion()}
+        </div>
+    );
+};
+
+export default HomePromotion;
