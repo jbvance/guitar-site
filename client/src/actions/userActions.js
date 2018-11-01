7d785ae92d2218620c122a56d3090d84d7e0c377
Jason Vance <=>
2018-11-01 06:09:16 -0500

    add route protection

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { USER_SERVER } from '../components/utils/misc';
 import {
     LOGIN_USER,
-    REGISTER_USER
+    REGISTER_USER,
+    AUTH_USER
 } from './types';
 
 export function registerUser(dataToSubmit) {
@@ -22,4 +23,13 @@ export function loginUser(dataToSubmit){
         type: LOGIN_USER,
         payload: request,
     }
+}
+
+export function auth() {
+    const request = axios.get(`${USER_SERVER}/auth`)
+    .then(response => response.data)
+    return {
+        type: AUTH_USER,
+        payload: request
+    };
 }
